3077d62daa018e744a6abdea2344754d86e1ab70
shota <s8152.45@gmail.com>
2025-06-04 10:39:40 +0900

    変数の実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,21 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 100;
+		double num2 = 200;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		String startText1 = "私の好きな整数は";
+		String startText2 = "私の好きな少数は";
+		String endText = "です";
+		
+		System.out.println( startText1 + num1 + endText);
+		System.out.println( startText2 + num2 + endText);
+	}
+
+}
