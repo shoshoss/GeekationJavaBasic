7a6782ee79b3d11d30c0c50712e2130b460a9bcf
shota <s8152.45@gmail.com>
2025-06-05 17:47:09 +0900

    Calculation2.javaファイルの作成と今までの演習内容をsrc直下へ

---
+++ src/basicSyntax/Calculation2.java
@@ -0,0 +1,19 @@
+package basicSyntax;
+
+public class Calculation2 {
+	public static int addUp(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public static int subtract(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public static int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public static int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+}
