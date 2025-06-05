5d8b069dda2f5544d08e4f50149f62f8e15efd0a
shota <s8152.45@gmail.com>
2025-06-05 18:01:08 +0900

    演習の実装

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -3,6 +3,17 @@ package basicSyntax;
 public class Main {
 
 	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
 		
+		int addUpResult = Calculation2.addUp(num1, num2);
+		int subtractResult = Calculation2.subtract(num1, num2);
+		int multiplyResult = Calculation2.multiply(num1, num2);
+		int divideResult = Calculation2.divide(num1, num2);
+		
+		System.out.println("足し算の計算結果は" + addUpResult + "です。");
+		System.out.println("引き算の計算結果は" + subtractResult + "です。");
+		System.out.println("掛け算の計算結果は" + multiplyResult + "です。");
+		System.out.println("割り算の計算結果は" + divideResult + "です。");
 	}
 }
