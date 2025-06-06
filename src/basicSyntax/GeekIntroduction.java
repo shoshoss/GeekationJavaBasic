85736f427efcaae529237c0cee173f9800982e62
shota <s8152.45@gmail.com>
2025-06-06 09:44:26 +0900

    クラスの実装の修正

--- src/basicSyntax/GeekIntroduction.java
+++ src/basicSyntax/GeekIntroduction.java
@@ -10,31 +10,29 @@ public class GeekIntroduction {
 	
 	// 特技メソッド
 	public static void specialSkill(int num1, int num2) {
+		if (num1 < 1 || num2 < 1) {
+			System.out.println("num1とnum2は0より大きい値にしてください。");
+			return;
+		}
+		if (num1 >= num2) {
+			System.out.println("num2はnum1より大きい値にしてください。");
+			return;
+		}
+		if (num1 > 300 || num2 > 300) {
+			System.out.println("num1とnum2は300以下にしてください。");
+			return;
+		}
+
 		for (int num = num1; num <= num2; num++) {
-			if (num1 < 1 || num2 < 1) {
-				System.out.println("num1とnum2は0より大きい値にしてください。");
-				break;
-			} else if (num1 >= num2) {
-				System.out.println("num2はnum1より大きい値にしてください");
-				break;
-			} else if (num1 > 300 || num2 > 300) {
-				System.out.println("num1とnum2は300以下にしてください。");
-				break;
-			} else if (num % 3 == 0 && num % 5 == 0) {
-				System.out.println(num + "は3の倍数かつ5の倍数です");
-				continue;
+			if (num % 3 == 0 && num % 5 == 0) {
+				System.out.println(num + "は3の倍数かつ5の倍数です。");
 			} else if (num % 3 == 0) {
-				System.out.println(num + "は3の倍数です");
-				continue;
+				System.out.println(num + "は3の倍数です。");
 			} else if (num % 5 == 0) {
-				System.out.println(num + "は5の倍数です");
-				continue;
+				System.out.println(num + "は5の倍数です。");
 			} else {
 				System.out.println(num);
 			}
-			
 		}
 	}
-	
-	
 }
